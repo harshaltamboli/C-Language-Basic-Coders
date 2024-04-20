800675e1a0afbb63a0b86a8bcc6e556777bc0ade
harshaltamboli <harshaltamboli429@gmail.com>
2024-04-20 21:43:40 +0530

    [ADDED]: harshal this is use a function

---
+++ factorial - Copy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int factorial(int n) {
+ 
+    if (n == 0)
+        return 1;
+   
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+    
+   
+    if (num < 0) {
+        printf("Error: Factorial is not defined for negative numbers.\n");
+        return 1; 
+    }
+
+    printf("Factorial of %d = %d\n", num, factorial(num));
+
+    return 0;
+}
+
